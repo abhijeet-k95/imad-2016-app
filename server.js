d2e5a56e596b6bafdd437d666064ae7ec04595c8
abhijeet-k95 <abhijeet.kurade95@gmail.com>
2016-10-15 09:27:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/signup_new_php', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'signup_new_php.php'));
 });
 
 
