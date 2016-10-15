f36e556916312c51e65259ddec497c659f2c8541
abhijeet-k95 <abhijeet.kurade95@gmail.com>
2016-10-15 09:17:27 +0530

    Add files via upload

---
+++ ui/signup_new_html.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+ <html> 
+    <head> 
+	<title>Sign-Up</title> 
+	<link rel="stylesheet" type="text/css" href="signup_new_style.css">
+	</head> 
+	
+	<body id="body-color"> 
+	<div id="Sign-Up"> 
+	<fieldset style="width:30%">
+	<legend>Registration Form</legend> 
+	<table border="0"> <tr> 
+	<form method="POST" action="signup_new_php.php"> 
+	    <td>First Name:</td><td> <input type="text" name="fname"></td> </tr> <tr> 
+		<td>Last Name:</td><td> <input type="text" name="lname"></td> </tr> <tr>
+		<td>Mobile:</td><td> <input type="text" name="mobile"></td> </tr> <tr> 
+		<td>UserName:</td><td> <input type="text" name="username"></td> </tr> <tr> 
+		<td>Password:</td><td> <input type="password" name="password"></td> </tr> <tr> 
+		<td>Confirm Password: </td><td><input type="password" name="cpass"></td> </tr> 
+		<tr> 
+		<td><input id="button" type="submit" name="submit" value="Sign-Up"></td> 
+		</tr> 
+		</form>
+	</table> 
+	</fieldset> 
+	</div> 
+	</body> 
+	</html>
+<?php
+
+?>  
